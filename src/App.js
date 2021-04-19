8e3794b4ac65fc7ef8ff4be64a75b6a4ea3427cf
Rico Putra Pradana <rico.putra@outlook.co.id>
2021-04-19 21:27:46 +0700

    #3.1 implement populate jobs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import { createContext, useEffect, useReducer } from 'react';
 import './App.css';
 import Card from './reusable-components/Card';
+import initialState from './state/initialState';
+import reducer from './state/reducer';
+
+export const MainContext = createContext();
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    fetch('data.json')
+      .then(res => res.json())
+      .then(res => {
+        dispatch({
+          type: 'populateJobs',
+          value: res
+        })
+      })
+  }, []);
+
   return (
     <div className="App">
-      <Card>
-        CARD
-      </Card>
+      <MainContext.Provider value={{ state, dispatch }}>
+        { state.jobs.map(job => (
+          <Card key={ job.id } isFeatured={ job.isFeatured }>
+            { job.position }
+          </Card>
+        )) }
+        <Card>
+          CARD
+        </Card>
+      </MainContext.Provider>
     </div>
   );
 }
--- src/state/reducer.js
+++ src/state/reducer.js
@@ -1,5 +1,13 @@
 const reducer = (state, action) => {
   switch (action.type) {
+    case 'populateJobs':
+      const allJobs = action.value;
+      
+      return {
+        ...state,
+        jobs: allJobs
+      }
+
     default:
       return state;
   }
